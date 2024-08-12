59511982da00ad2edd0df627406d835206c4422f
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-08-11 23:05:15 -0300

    segundo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -27,10 +27,7 @@ export default function Home() {
                 height="315"
                 src="https://www.youtube.com/embed/mr2pfZZREOg?si=sDTvKSsCM8LmJDnp"
                 title="YouTube video player"
-                frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin"
-                allowfullscreen
             ></iframe>
         </div>
     );
