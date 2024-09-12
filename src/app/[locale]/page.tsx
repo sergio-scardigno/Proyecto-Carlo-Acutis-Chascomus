050ab1310d1da2f51506b4e104699c45faede9e0
Sergio <sergioscardigno82@gmail.com>
2024-09-12 11:40:24 -0300

    modificacion

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             </p>
             
 
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/9UHes5e8gU4?si=hMZkB-VvdZ9aWXy5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/9UHes5e8gU4?si=hMZkB-VvdZ9aWXy5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
 
 
             <hr className="w-full border-t border-gray-300 my-4" />
