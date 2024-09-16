9f518b81296b27e434d4ebe4625fc6c0858a46eb
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-09-16 12:03:13 -0300

    sign

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,52 @@
+// import { useTranslations } from 'next-intl';
+
+// export default function Footer() {
+//     const t = useTranslations('Footer');
+
+//     return (
+//         <div className="my-10 text-center">
+//             <p>{t('copyright')}</p>
+//             <p>
+//                 <a
+//                     href="https://cv-sergio-scardigno.vercel.app/"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                     className="text-blue-600 hover:underline"
+//                 >
+//                     {t('portfolio')}
+//                 </a>
+//             </p>
+//         </div>
+//     );
+// }
+
 import { useTranslations } from 'next-intl';
 
 export default function Footer() {
-  const t = useTranslations('Footer');
+    const t = useTranslations('Footer');
 
-  return (
-    <div className='my-10 text-center'>
-      <p>{t('copyright')}</p>
-    </div>
-  );
+    return (
+        <div className="my-10 text-center">
+            <p>{t('copyright')}</p>
+            <p className="mt-5">
+                <a
+                    href="https://cv-sergio-scardigno.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="custom-button"
+                >
+                    <span className="icon">
+                        {/* Aquí puedes usar un ícono o imagen */}
+                        <img
+                            src="/img/programmer.png"
+                            alt="icon"
+                            width={16}
+                            height={16}
+                        />
+                    </span>
+                    <span className="text-footer-sign">{t('portfolio')}</span>
+                </a>
+            </p>
+        </div>
+    );
 }
