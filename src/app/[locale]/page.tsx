119c28a23fe37cca69495c5cf7bf75f2bfe7e31e
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-09-28 13:28:50 -0300

    entroncacion-imagen

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -31,22 +31,19 @@ export default function Home() {
             />
 
             <hr className="w-full border-t border-gray-300 my-4" />
+
+            <Image
+                src="/img/entronacion.jpg"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                className="rounded-lg shadow-lg"
+            />
+
             <div style={{ maxWidth: '600px', margin: '0 auto' }}>
                 <p className="text-center mt-4 mb-4">{t('ceremony')}</p>
             </div>
             <Timeline />
-
-            <hr className="w-full border-t border-gray-300 my-4" />
-            <p className="text-center text-base mb-4 mt-4">
-                {t('bibliography-intro')}
-            </p>
-            <iframe
-                width="auto"
-                height="auto"
-                src="https://www.youtube.com/embed/3IQF38Icrgw?si=EthKfFau--vgBseL"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            ></iframe>
 
             <hr className="w-full border-t border-gray-300 my-4" />
             <Image
@@ -57,31 +54,6 @@ export default function Home() {
                 className="rounded-lg shadow-lg"
             />
 
-            <hr className="w-full border-t border-gray-300 my-4" />
-            <p className="text-center text-2xl font-semibold mt-4">
-                {t('who-is')}
-            </p>
-
-            <iframe
-                width="auto"
-                height="auto"
-                src="https://www.youtube.com/embed/9UHes5e8gU4?si=hMZkB-VvdZ9aWXy5"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            ></iframe>
-
-            <hr className="w-full border-t border-gray-300 my-4" />
-            <p className="text-center text-2xl font-semibold mt-4">
-                {t('song-intro')}
-            </p>
-            <iframe
-                className="mt-8"
-                width="auto"
-                height="auto"
-                src="https://www.youtube.com/embed/mr2pfZZREOg?si=sDTvKSsCM8LmJDnp"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            ></iframe>
             <hr className="w-full border-t border-gray-300 my-4" />
         </div>
     );
--- src/app/[locale]/videos/page.tsx
+++ src/app/[locale]/videos/page.tsx
@@ -10,30 +10,73 @@ export default function Oracion() {
                 {t('title')}
             </h1>
 
+            {/* Primera fila de videos */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+                {/* Primer video */}
                 <div className="flex flex-col items-center">
-                    <p className="text-base font-bold mb-4 text-gray-800 tracking-wider drop-shadow-lg">{t('video1')}</p>
-                    <div className="w-full aspect-w-16 aspect-h-9">
-                    <iframe width="auto" height="auto" src="https://www.youtube.com/embed/euv57BSyAgg?si=KbFHMMsVWdg8pxk_" 
-                    title="YouTube video player"  
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    ></iframe>
-                    </div>
-                </div>
-
-                {/* <div className="flex flex-col items-center">
-                    <h2 className="text-lg font-semibold mb-4">{t('video2')}</h2>
+                    <p className="text-base font-bold mb-4 text-gray-800 tracking-wider drop-shadow-lg">
+                        {t('video1')}
+                    </p>
                     <div className="w-full aspect-w-16 aspect-h-9">
                         <iframe
                             className="w-full h-full"
-                            src="https://www.youtube.com/embed/video2URL"
+                            src="https://www.youtube.com/embed/euv57BSyAgg?si=KbFHMMsVWdg8pxk_"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         ></iframe>
                     </div>
-                </div> */}
+                </div>
 
-                {/* Agrega más videos como sea necesario */}
+                {/* Segundo video */}
+                <div className="flex flex-col items-center">
+                    <p className="text-base font-bold mb-4 text-gray-800 tracking-wider drop-shadow-lg">
+                        {t('song-intro')}
+                    </p>
+                    <div className="w-full aspect-w-16 aspect-h-9">
+                        <iframe
+                            className="w-full h-full"
+                            src="https://www.youtube.com/embed/mr2pfZZREOg?si=sDTvKSsCM8LmJDnp"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        ></iframe>
+                    </div>
+                </div>
+            </div>
+
+            {/* Separador */}
+            <hr className="w-full border-t border-gray-300 my-8" />
+
+            {/* Segunda fila de videos */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+                {/* Tercer video */}
+                <div className="flex flex-col items-center">
+                    <p className="text-base font-bold mb-4 text-gray-800 tracking-wider drop-shadow-lg">
+                        {t('who-is')}
+                    </p>
+                    <div className="w-full aspect-w-16 aspect-h-9">
+                        <iframe
+                            className="w-full h-full"
+                            src="https://www.youtube.com/embed/9UHes5e8gU4?si=hMZkB-VvdZ9aWXy5"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        ></iframe>
+                    </div>
+                </div>
+
+                {/* Cuarto video */}
+                <div className="flex flex-col items-center">
+                    <p className="text-base font-bold mb-4 text-gray-800 tracking-wider drop-shadow-lg">
+                        {t('bibliography-intro')}
+                    </p>
+                    <div className="w-full aspect-w-16 aspect-h-9">
+                        <iframe
+                            className="w-full h-full"
+                            src="https://www.youtube.com/embed/mr2pfZZREOg?si=sDTvKSsCM8LmJDnp"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        ></iframe>
+                    </div>
+                </div>
             </div>
         </div>
     );
