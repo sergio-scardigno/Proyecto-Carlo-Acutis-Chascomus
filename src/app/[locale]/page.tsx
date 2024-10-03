dc46eb2929f9c84720f3737802719598c71c22e5
Sergio <sergioscardigno82@gmail.com>
2024-10-03 11:35:20 -0300

    change

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import './globals.css';
 import Timeline from '../../components/timeline-carlo-acutis';
 import { useEffect, useState } from 'react';
-import Head from 'next/head'; // Importar Head
+// import Head from 'next/head'; // Importar Head
 
 export default function Home() {
     const t = useTranslations('IndexPage');
@@ -17,100 +17,63 @@ export default function Home() {
     }, []);
 
     return (
-        <>
-            <Head>
-                <title>Carlo Acutis en Chascomús - Proyecto Carlo Acutis</title>
-                <meta
-                    name="description"
-                    content="Conoce la vida y devoción de Carlo Acutis en Chascomús. Información sobre su testimonio de fe, eventos religiosos, y recursos espirituales en la ciudad."
-                />
-                <meta
-                    name="keywords"
-                    content="Carlo Acutis, Beato, Iglesia, Carlo Acutis Chascomús, Vida de Santos, Devoción, Testimonio de Fe"
-                />
-                <meta name="author" content="Sergio" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta
-                    property="og:title"
-                    content="Carlo Acutis en Chascomús - Proyecto Carlo Acutis"
-                />
-                <meta
-                    property="og:description"
-                    content="Descubre la devoción a Carlo Acutis en Chascomús y cómo su vida inspira a creyentes de todas las edades."
-                />
-                <meta property="og:image" content="/img/carlo-acutis.webp" />
-                <meta
-                    property="og:url"
-                    content="https://www.carloacutis.net.ar/es/"
-                />
-                <meta name="twitter:card" content="summary_large_image" />
-                <link
-                    rel="canonical"
-                    href="https://www.carloacutis.net.ar/es"
-                />
-            </Head>
+        <div className="flex flex-col items-center justify-center min-h-screen p-4 hero">
+            <h1 className="mb-4 font-semibold text-center font-zapfino">
+                {t('title')}
+            </h1>
+            <p className="text-center mb-4">{t('description')}</p>
+            <hr className="w-full border-t border-gray-300 my-4" />
+            <Image
+                src="/img/carlo-acutis.webp"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                className="rounded-lg shadow-lg"
+            />
 
-            <div className="flex flex-col items-center justify-center min-h-screen p-4 hero">
-                <h1 className="mb-4 font-semibold text-center font-zapfino">
-                    {t('title')}
-                </h1>
-                <p className="text-center mb-4">{t('description')}</p>
-                <hr className="w-full border-t border-gray-300 my-4" />
-                <Image
-                    src="/img/carlo-acutis.webp"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                    className="rounded-lg shadow-lg"
-                />
+            <hr className="w-full border-t border-gray-300 my-4" />
 
-                <hr className="w-full border-t border-gray-300 my-4" />
+            <Image
+                src="/img/entronacion.jpg"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                className="rounded-lg shadow-lg"
+            />
 
-                <Image
-                    src="/img/entronacion.jpg"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                    className="rounded-lg shadow-lg"
-                />
-
-                <div style={{ maxWidth: '600px', margin: '0 auto' }}>
-                    <p className="text-center mt-4 mb-4">{t('ceremony')}</p>
-                </div>
-                <Timeline />
-
-                <hr className="w-full border-t border-gray-300 my-4" />
-                <div style={{ maxWidth: '600px', margin: '0 auto' }}>
-                    <h2 className="font-semibold text-center mt-4 mb-4">
-                        {t('monsenor')}
-                    </h2>
-                    <p className="text-center mt-4 mb-4 text-justify">
-                        {t('description2')}
-                    </p>
-                </div>
-                <iframe
-                    className="w-full"
-                    height={500}
-                    src="https://www.youtube.com/embed/IpkpJDSHfTg?si=JHq-gc4QOnvrosm0"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                ></iframe>
-
-                <hr className="w-full border-t border-gray-300 my-4" />
-
-                <Image
-                    src="/img/folleto.webp"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                    className="rounded-lg shadow-lg"
-                />
-
-                <hr className="w-full border-t border-gray-300 my-4" />
+            <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+                <p className="text-center mt-4 mb-4">{t('ceremony')}</p>
             </div>
-        </>
+            <Timeline />
+
+            <hr className="w-full border-t border-gray-300 my-4" />
+            <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+                <h2 className="font-semibold text-center mt-4 mb-4">
+                    {t('monsenor')}
+                </h2>
+                <p className="text-center mt-4 mb-4 text-justify">
+                    {t('description2')}
+                </p>
+            </div>
+            <iframe
+                className="w-full"
+                height={500}
+                src="https://www.youtube.com/embed/IpkpJDSHfTg?si=JHq-gc4QOnvrosm0"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+
+            <hr className="w-full border-t border-gray-300 my-4" />
+
+            <Image
+                src="/img/folleto.webp"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                className="rounded-lg shadow-lg"
+            />
+
+            <hr className="w-full border-t border-gray-300 my-4" />
+        </div>
     );
 }
