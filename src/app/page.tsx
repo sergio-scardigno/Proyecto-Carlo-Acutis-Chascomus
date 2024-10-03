6fda90aaaf9788636ebcc6bc83569f36bc47922a
Sergio <sergioscardigno82@gmail.com>
2024-10-03 12:38:49 -0300

    cambio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
-'use client';
+'use client'; // Asegúrate de que este componente es un Client Component
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function RootPage() {
     const router = useRouter();
 
     useEffect(() => {
-        // Verifica que se esté ejecutando en el lado del cliente
-        if (typeof window !== 'undefined') {
-            // Obtener el idioma del navegador
-            const userLang = navigator.language; // Usa 'language' en lugar de 'userLanguage'
-            if (userLang.startsWith('en')) {
-                router.push('/en'); // Redirigir a la versión en inglés
-            } else {
-                router.push('/es'); // Redirigir a la versión en español
-            }
+        const userLang = navigator.language || navigator.userLanguage; // Usa 'language' en lugar de 'userLanguage'
+        if (userLang.startsWith('en')) {
+            router.push('/en'); // Redirigir a la versión en inglés
+        } else {
+            router.push('/es'); // Redirigir a la versión en español
         }
     }, [router]);
 
