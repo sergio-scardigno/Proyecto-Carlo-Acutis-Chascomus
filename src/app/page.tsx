68bd71447dca622c7a8653a1bc7562f637fe49a8
Sergio <sergioscardigno82@gmail.com>
2024-10-03 12:34:50 -0300

    cambio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import { redirect } from 'next/navigation';
-import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function RootPage() {
-    return (
-        <>
-            <Head>
-                <meta name="robots" content="noindex" />
-            </Head>
-        </>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        // Verifica que se esté ejecutando en el lado del cliente
+        if (typeof window !== 'undefined') {
+            // Obtener el idioma del navegador
+            const userLang = navigator.language; // Usa 'language' en lugar de 'userLanguage'
+            if (userLang.startsWith('en')) {
+                router.push('/en'); // Redirigir a la versión en inglés
+            } else {
+                router.push('/es'); // Redirigir a la versión en español
+            }
+        }
+    }, [router]);
+
+    return <div>Cargando...</div>; // Mensaje de carga
 }
